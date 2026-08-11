20027c4491330232aca718e7b7e81ab4f1e1714b
FrancoSecchi <francofabiansecchi@gmail.com>
2026-08-11 04:25:36 -0300

    Duplicacion archivo + implementacion de conjuntos

--- tp0.py
+++ tp0.py
@@ -16,11 +16,16 @@ def esprimo(n):
 def getNumOfIterations(n: int) -> int :
     return math.floor((n - 19) * 1/30)
 
-def prog(num: int):
+### {30n + 11, 30n + 13, 30n + 17, 30n + 19} esto me generá potenciales grupos pero no me asegura que cada uno de sus elementos sea primo. Esta estructura es necesaria para asegurar que ninguno de los cuatro números primos sea divisible por 2, 3 o por 5).
+def prog(nIter: int):
     t1=time.time()
-    for i in range (11, num):
-        if esprimo(i) and esprimo(i+2) and esprimo(i+6) and esprimo(i+8):
-            print (i, i+2, i+6,i+8)
+    for i in range (0, nIter + 1):
+        n1 = 30*i +11
+        n2 = 30*i + 13
+        n3 = 30*i + 17
+        n4 = 30*i + 19
+        if esprimo(n1) and esprimo(n2) and esprimo(n3) and esprimo(n4):
+            print(n1, n2, n3, n3)
     t2=time.time()
     print(t2-t1)
 
@@ -38,7 +43,7 @@ if __name__ == "__main__":
 
     num_iterations = getNumOfIterations(num)
     print(num_iterations)
-  ##  prog(num)
+    prog(num_iterations)
 
 
 
