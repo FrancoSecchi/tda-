677bcc0acef6e5a180c18adde43c111c2e004823
FrancoSecchi <francofabiansecchi@gmail.com>
2026-08-11 01:43:43 -0300

    initial tp

---
+++ tp0.py
@@ -0,0 +1,32 @@
+import time, sys
+
+def esprimo(n):    
+    i = 2
+    cd=0
+    while i<=n-1:
+        if n%i == 0:
+            cd = cd+1
+        i = i+1
+    if cd ==0:
+        return True
+    else:
+        return False
+
+def prog(num: int):
+    t1=time.time()
+    for i in range (11,1000000):
+        if esprimo(i) and esprimo(i+2) and esprimo(i+6) and esprimo(i+8):
+            print (i, i+2, i+6,i+8)
+    t2=time.time()
+    print(t2-t1)
+
+if __name__ == "__main__": 
+    args = sys.argv
+    if len(args) - 2 < 0:
+        print("Argumentos insuficientes")
+        exit()
+    num = args[1]
+
+    #prog()
+
+
